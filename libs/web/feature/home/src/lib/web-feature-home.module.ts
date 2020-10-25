9a47f155b04ba04184756c83af40400c0683e47e
Preston Lamb <preston.j.lamb@gmail.com>
2020-10-25 14:12:30 -0600

    added homepages for the web and admin apps

--- libs/web/feature/home/src/lib/web-feature-home.module.ts
+++ libs/web/feature/home/src/lib/web-feature-home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   imports: [
@@ -10,5 +11,6 @@ import { RouterModule } from '@angular/router';
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [HomepageComponent],
 })
 export class WebFeatureHomeModule {}
